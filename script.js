3435b43f41846d74705d9c057097d85807f678ee
Wernajal <wyattbray465@gmail.com>
2026-06-08 10:40:23 +0300

    Add fetch request to contact endpoint

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log("Welcome to Bravin Mulati's Portfolio!");
 alert("Hello! Thanks for visiting my website.");
+fetch("http://127.0.0.1:3000/contact", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, message })
+})
